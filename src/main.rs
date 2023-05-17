7bbc74564e4e68a7d29750c6a0cb2ab212d0eb71
rafael <rrrr0ffff@gmail.com>
2023-05-17 16:39:50 -0300

    get content of file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("{:?}", args);
+    let content = std::fs::read_to_string(&args[2]).expect("Error reading the file");
+    println!("FILENAME: {}", content);
 }
