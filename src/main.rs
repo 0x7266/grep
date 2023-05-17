92d3db8e5732c4e91faa24ee539c9bba21f7b681
rafael <rrrr0ffff@gmail.com>
2023-05-17 19:39:00 -0300

    refactoring: parse_config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
-    let content = std::fs::read_to_string(&args[2]).expect("Error reading the file");
+    let (query, filename) = parse_config(&args);
+    let content = std::fs::read_to_string(filename).expect("Error reading the file");
     println!("FILENAME: {}", content);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    (&args[1], &args[2])
 }
