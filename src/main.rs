68861ed157c1574b20337f71d9c5abcb50fb4f98
rafael <rrrr0ffff@gmail.com>
2023-05-17 20:07:01 -0300

    error handling with unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
-        panic!("Not enough arguments")
-    }
-    let input: Input = Input::new(&args);
+    let input: Input = Input::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
+        std::process::exit(1) // exit without panic
+    });
     let content = std::fs::read_to_string(input.filename).expect("Error reading the file");
     println!("FILENAME: {}", content);
 }
@@ -14,10 +14,13 @@ struct Input {
 }
 
 impl Input {
-    fn new(args: &[String]) -> Input {
-        Input {
+    fn new(args: &[String]) -> Result<Input, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        Ok(Input {
             query: args[1].clone(),
             filename: args[2].clone(),
-        }
+        })
     }
 }
