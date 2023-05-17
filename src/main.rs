a785725059ed5adfc5f069fcf375da87d68782ed
rafael <rrrr0ffff@gmail.com>
2023-05-17 19:48:20 -0300

    impl input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let input: Input = parse_config(&args);
+    let input: Input = Input::new(&args);
     let content = std::fs::read_to_string(input.filename).expect("Error reading the file");
     println!("FILENAME: {}", content);
 }
@@ -10,9 +10,11 @@ struct Input {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Input {
-    Input {
-        query: args[1].clone(),
-        filename: args[2].clone(),
+impl Input {
+    fn new(args: &[String]) -> Input {
+        Input {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
     }
 }
