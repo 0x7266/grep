f7b182ec98cfaca4fb621b86c6f0f98d61912b84
rafael <rrrr0ffff@gmail.com>
2023-05-18 19:06:47 -0300

    writing to standard error

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input: Input = Input::new(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err); // now if an error occurs the output will be blank (eg. cargo run > output.txt)
         process::exit(1) // exit without panic
     });
     if let Err(err) = minigrep::run(input) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
         process::exit(1);
     }
 }
