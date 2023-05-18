a62e7ab31db416f45051b189a52cbd74f15c8aa9
rafael <rrrr0ffff@gmail.com>
2023-05-18 01:19:21 -0300

    search_case_insensitive test

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,19 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
-            results.push(line)
+            results.push(line);
+        }
+    }
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
         }
     }
     results
@@ -45,15 +57,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-        let query = "three";
+    fn case_sensitive() {
+        let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
-        assert_eq!(vec!["Pick three."], search(query, contents))
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 
-    fn search_case_insensitive() {}
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
+    }
 }
